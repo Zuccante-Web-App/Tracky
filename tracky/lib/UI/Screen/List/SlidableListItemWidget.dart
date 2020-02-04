964cff90bb61d10f90e5ff7c45585e304d2bf545
Thomas Stufano <thomas.stufano@gmail.com>
2020-02-04 15:48:02 +0100

    Added Tile Swap

--- tracky/lib/UI/Screen/List/SlidableListItemWidget.dart
+++ tracky/lib/UI/Screen/List/SlidableListItemWidget.dart
@@ -7,15 +7,19 @@ class SlidableListItemWidget extends StatefulWidget {
   String nome;
   int prezzo;
   int index;
+  GlobalKey<AnimatedListState> _productList;
+  List<Product> _myList;
+  IconData star = OMIcons.starBorder;
 
-  SlidableListItemWidget(this.nome, this.prezzo, this.index);
+  SlidableListItemWidget(
+      this.nome, this.prezzo, this.index, this._productList, this._myList);
 
   @override
-  _SlidableListItemWidgetState createState() =>
-      _SlidableListItemWidgetState();
+  _SlidableListItemWidgetState createState() => _SlidableListItemWidgetState();
 }
 
 class _SlidableListItemWidgetState extends State<SlidableListItemWidget> {
+  bool isFavourite = true;
   //Icon star = Icon(OMIcons.starBorder);
   @override
   Widget build(BuildContext context) {
@@ -37,20 +41,56 @@ class _SlidableListItemWidgetState extends State<SlidableListItemWidget> {
       ),
       actions: <Widget>[
         IconSlideAction(
-          caption: 'Delete',
-          color: Colors.red,
-          icon: OMIcons.delete,
-          onTap:(){
-            MySlidableListWidget.deleteProduct(widget.index);
-          }
-        ),
-        IconSlideAction(
+            caption: 'Delete',
+            color: Colors.red,
+            icon: OMIcons.delete,
+            onTap: delete),
+         IconSlideAction(
           caption: 'Favorite',
           color: Colors.amber[300],
-          icon: OMIcons.starBorder,
+          icon: widget.star,
+          onTap: makeFavorite,
         ),
       ],
     );
+  }
+
+  Widget _buildItem(Product product, Animation animation) {
+    return SizeTransition(
+      sizeFactor: animation,
+      child: Card(),
+    );
+  }
+
+  void delete() {
+    setState(() {
+      Product removedProduct = widget._myList.removeAt(widget.index);
+      AnimatedListRemovedItemBuilder builder = (context, animation) {
+        return _buildItem(removedProduct, animation);
+      };
+      widget._productList.currentState.removeItem(widget.index, builder);
+    });
+  }
+
+  void makeFavorite() {
+    print("ciao$isFavourite ");
+    setState(() {
+      /*if (isFavourite) {
+        print("entrato nel if");
+        isFavourite = false;
+        widget.star = OMIcons.starBorder;
+      } else {
+        print(isFavourite);*/
+        Product favoriteProduct = widget._myList[widget.index];
+        AnimatedListRemovedItemBuilder builder = (context, animation) {
+          return _buildItem(favoriteProduct, animation);
+        };
+        widget._productList.currentState.removeItem(widget.index, builder);
+        widget._myList.removeAt(widget.index);
+        widget._myList.insert(0, favoriteProduct);
+        widget._productList.currentState.insertItem(widget.index); 
+        //isFavourite = true;
+    });
   }
 }
 
--- tracky/lib/UI/Screen/List/SlidableListWidget.dart
+++ tracky/lib/UI/Screen/List/SlidableListWidget.dart
@@ -1,14 +1,11 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:tracky/UI/Screen/List/SlidableListItemWidget.dart';
 
-
 class MySlidableListWidget extends StatefulWidget {
   GlobalKey<AnimatedListState> _productList = GlobalKey<AnimatedListState>();
   List<String> _list;
-  List<Product> _myList = [new Product('TelefonoX', 500,0)];  
-
+  List<Product> _myList = [new Product('TelefonoX', 500), new Product('nome', 1221), new Product("p3", 111)];
 
   MySlidableListWidget({Key key, this.title}) : super(key: key);
   final String title;
@@ -18,41 +15,39 @@ class MySlidableListWidget extends StatefulWidget {
 }
 
 class _MySlidableListWidgetState extends State<MySlidableListWidget> {
-  String actualIndex = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Bacheca'),),
+      appBar: AppBar(title: Text('La tua lista')),
       body: Center(
         child: AnimatedList(
             key: widget._productList,
             initialItemCount: widget._myList.length,
             itemBuilder: (context, index, animation) {
               return ScaleTransition(
-                  scale: animation,
-                  
-                  child: SlidableListItemWidget(
-                      widget._myList[index].nome,
-                      widget._myList[index].prezzo,
-                      widget._myList[index].index
-              )
-                  );
+                scale: animation,
+                child: SlidableListItemWidget(
+                  widget._myList[index].nome,
+                  widget._myList[index].prezzo,
+                  index,
+                  widget._productList,
+                  widget._myList,
+                ),
+              );
             }),
       ),
     );
   }
-    void deleteProduct(int index){
-     widget._myList.removeAt(index);
-  }
 }
-
- 
 
 class Product {
   String nome;
   int prezzo;
   int index;
+  Key key;
 
-  Product(this.nome, this.prezzo,this.index);
+  Product(
+    this.nome,
+    this.prezzo,
+  );
 }
-
